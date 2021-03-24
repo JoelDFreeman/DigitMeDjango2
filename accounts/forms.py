dae3466040e7924f28d2b39fc2c044a8bc9523f9
JoelDFreeman <75079103+JoelDFreeman@users.noreply.github.com>
2021-03-24 02:38:05 -0700

    Add files via upload

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import *
 
@@ -22,3 +23,68 @@ class CreateUserForm(UserCreationForm):
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 
+class ProductForm(forms.ModelForm):
+	class Meta:
+		model = Product
+		fields = [
+			'name',
+			'price',
+			'category',
+			'description',
+			'product_note',
+			'productimage',
+			'value',
+			'qty',
+			'value',
+		]
+
+class OrderCreateForm(forms.ModelForm):
+	class Meta:
+		model = Order
+		fields = [
+			'customer',
+			'product',
+			'status',
+		]
+
+class TagCreateForm(forms.ModelForm):
+	class Meta:
+		model = Tag
+		fields = [
+			'name',
+		]
+
+class CustomerCreateForm(forms.ModelForm):
+	class Meta:
+		model = Customer
+		fields = [
+			'name',
+			'phone',
+			'email',
+			'profile_pic',
+			'customeraddress'
+		]
+
+class CustomerAddressCreateForm(forms.ModelForm):
+	class Meta:
+		model = CustomerAddress
+		fields = [
+			'addressline1',
+			'county',
+			'postcode',
+		]		
+
+class ProductImageForm(forms.ModelForm):
+	class Meta:
+		model = ProductImage
+		fields = [
+			'name',
+			'product_Main_Img',
+		]	
+
+class UserListView(forms.ModelForm):
+    model = Customer
+    template_name = 'customers.html'  # Default: <app_label>/<model_name>_list.html
+    context_object_name = 'customers'  # Default: object_list
+    paginate_by = 5
+    queryset = Customer.objects.all()  # Default: Model.objects.all()			
