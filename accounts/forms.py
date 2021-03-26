facf01cbf97ff1b92c01bccceb9046458fec2723
JoelDFreeman <75079103+JoelDFreeman@users.noreply.github.com>
2021-03-26 04:42:02 -0700

    Add files via upload

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import *
 
@@ -20,5 +21,63 @@ class OrderForm(ModelForm):
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+		fields = '__all__'
+
+class ProductForm(forms.ModelForm):
+	class Meta:
+		model = Product
+		fields = '__all__'
+
+class OrderCreateForm(forms.ModelForm):
+	class Meta:
+		model = Order
+		fields = '__all__'
+
+	note = forms.CharField(widget=forms.Textarea)
+	note2 = forms.CharField(widget=forms.Textarea)
+
+class TagCreateForm(forms.ModelForm):
+	class Meta:
+		model = Tag
+		fields = '__all__'
+
+class CustomerAddressForm(forms.ModelForm):
+	class Meta:
+		model = CustomerAddress
+		fields = [
+			'name',
+			'addressline1',
+			'county',
+			'postcode',
+		]	
+
+class CustomerCreateForm(forms.ModelForm):
+	class Meta:
+		customeraddress= forms.ChoiceField(widget=forms.RadioSelect, required=True)
+		model = Customer
+		fields = [
+			'name',
+			'phone',
+			'email',
+ 			'profile_pic',
+			'customeraddress',
+	
+		]
+	
+
+
+class ProductImageForm(forms.ModelForm):
+	class Meta:
+		model = ProductImage
+		fields = [
+			'name',
+			'product_Main_Img',
+		]	
+
+class UserListView(forms.ModelForm):
+    model = Customer
+    template_name = 'customers.html'  # Default: <app_label>/<model_name>_list.html
+    context_object_name = 'customers'  # Default: object_list
+    paginate_by = 5
+    queryset = Customer.objects.all()  # Default: Model.objects.all()	
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -9,17 +12,40 @@ urlpatterns = [
 
     path('', views.home, name="home"),
     path('user/', views.userPage, name="user-page"),
-
+    path('upload/', views.upload, name="upload"),
     path('account/', views.accountSettings, name="account"),
 
     path('products/', views.products, name='products'),
+    path('product_create/', views.product_create, name='product_create'),
+    path('update_product/<str:pk>/', views.updateProduct, name="update_product"),
+    path('delete_product/<str:pk>/', views.deleteProduct, name="delete_product"),
+    
     path('customer/<str:pk_test>/', views.customer, name="customer"),
+    path('customers/', views.customers, name="customers"),
+    path('update_customer/<str:pk>/', views.updateCustomer, name="update_customer"),
+    path('customer_create/', views.customer_create, name='customer_create'),
+    path('delete_customer/<str:pk>/', views.deleteCustomer, name="delete_customer"),
+
     path('orders/', views.Orders, name="orders"),
+    path('order_create/', views.order_create, name='order_create'),
+
+
+    path('tag_create/', views.tag_create, name='tag_create'),
+
+    path('visualisations/', views.visualisations, name='visualisations'),
 
     path('create_order/<str:pk>/', views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
-    path('create_customer/<str:pk>/', views.createCustomer, name="create_customer"),
 
+    path('create_address/', views.createAddress, name="create_address"),
+    path('update_address/<str:pk>/', views.updateAddress, name="update_address"),
+    path('delete_address/<str:pk>/', views.deleteAddress, name="delete_address"),
 
+    path('export/',views.download_csv,name='export'),
+    
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
