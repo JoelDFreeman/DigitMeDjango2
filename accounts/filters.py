ea480e87878710bb21a65c2df961604866762de0
JoelDFreeman <75079103+JoelDFreeman@users.noreply.github.com>
2021-03-29 06:35:12 -0700

    Add files via upload

--- accounts/filters.py
+++ accounts/filters.py
@@ -13,3 +13,30 @@ class OrderFilter(django_filters.FilterSet):
 		model = Order
 		fields = '__all__'
 		exclude = ['customer', 'date_created']
+
+class ProductFilter(django_filters.FilterSet):
+	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
+	end_date = DateFilter(field_name="date_created", lookup_expr='lte')
+	note = CharFilter(field_name='note', lookup_expr='icontains')
+
+
+	class Meta:
+		model = Product
+		fields = '__all__'
+		exclude = ['date_created']
+
+class CustomerFilter(django_filters.FilterSet):
+	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
+	end_date = DateFilter(field_name="date_created", lookup_expr='lte')
+	note = CharFilter(field_name='note', lookup_expr='icontains')
+
+
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['date_created', 'profile_pic', 'note']		
+
+
+
+
+				
